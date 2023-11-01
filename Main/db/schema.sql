b586087ec86a0ff49e4223be65467b67dee7eda1
Milbrun Pearson <milbrunpearson@Milbruns-MacBook-Air-2.local>
2023-10-31 20:26:23 -0400

    added sql

---
+++ Main/db/schema.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE employee_tracker;
+
+   CREATE TABLE departments (
+       id INT AUTO_INCREMENT PRIMARY KEY,
+       name VARCHAR(30) NOT NULL
+   );
+
+   CREATE TABLE roles (
+       id INT AUTO_INCREMENT PRIMARY KEY,
+       title VARCHAR(30) NOT NULL,
+       salary DECIMAL(9,2) NOT NULL,
+       department_id INT,
+       FOREIGN KEY (department_id) REFERENCES departments(id)
+   );
+
+   CREATE TABLE employees (
+       id INT AUTO_INCREMENT PRIMARY KEY,
+       first_name VARCHAR(30) NOT NULL,
+       last_name VARCHAR(30) NOT NULL,
+       role_id INT,
+       manager_id INT,
+       FOREIGN KEY (role_id) REFERENCES roles(id),
+       FOREIGN KEY (manager_id) REFERENCES employees(id)
+   );
